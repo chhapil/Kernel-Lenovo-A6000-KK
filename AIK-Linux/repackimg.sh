b93e904fb29bae789e330a4eb5639853fcb1c0e9
Ruturaj <ru2raj@gmail.com>
2015-12-30 11:21:52 +0530

    clear statement removed to see logs in repacking image

--- AIK-Linux/repackimg.sh
+++ AIK-Linux/repackimg.sh
@@ -17,7 +17,7 @@ if [ -z "$(ls split_img/* 2> /dev/null)" -o -z "$(ls ramdisk/* 2> /dev/null)" ];
   exit 1;
 fi;
 
-clear;
+#clear;
 echo " ";
 echo "Android Image Kitchen - RepackImg Script";
 echo "by osm0sis @ xda-developers";
